ce3127d54e9240ed7399da836ccf2ec28e2cb9c4
Erick Paredes Win <erikparedes246@gmail.com>
2024-03-17 15:54:19 -0500

    actualizaacion de css
    vamos muy bien

---
+++ reutilizables/baseData/datos.js
@@ -0,0 +1,110 @@
+export const contenidoData = [
+        {
+                titulo: 'Joker: Follie a deux',
+                actor: 'Joaquin Phoenix',
+                precio: '50,00',
+                enlace: 'https://es.wikipedia.org/wiki/Joker:_Folie_%C3%A0_Deux',
+                imagen: 'img/joker.png'
+        },
+        {
+                titulo: 'Deadpool 3',
+                actor: ' Ryan Reynolds',
+                precio: '25,00',
+                enlace: 'https://www.espinof.com/fecha-de-estreno/deadpool-3-fecha-estreno-trailer-reparto-pelicula-marvel-protagonizada-ryan-reynolds',
+                imagen: 'img/deadpool.png'
+        },
+        {
+                titulo: 'Venom 3',
+                actor: 'Tom Hardy',
+                precio: '100,00',
+                enlace: 'https://www.fotogramas.es/noticias-cine/a37978794/venom-3-fecha-estreno-argumento-reparto-trailer/',
+                imagen: 'img/venom.png'
+        },
+        {
+                titulo: 'Wonder Woman 3',
+                actor: 'Gal Gadot',
+                precio: '70,00',
+                enlace: 'https://www.fotogramas.es/noticias-cine/a37983052/wonder-woman-3-gal-gadot-sinopsis-fecha-estreno-reparto-trailer/',
+                imagen: 'img/wonderwoman.png'
+        }
+]
+export const PeliculasData = [
+        {
+                imagen: 'img/datosPeliculas/Capitán América El primer vengador.jpg',
+                nombre: 'Capitán América: El primer vengador',
+                descripcion: 'Nacido durante la Gran Depresión, Steve Rogers creció como un chico enclenque en una familia pobre. Horrorizado por las noticias que llegaban de Europa sobre los nazis, decidió enrolarse en el ejército; sin embargo, debido a su precaria salud, fue rechazado una y otra vez. Enternecido por sus súplicas, el General Chester Phillips le ofrece la oportunidad de tomar parte en un experimento especial.',
+                precio: '10.00'
+        },
+        {
+                imagen: 'img/datosPeliculas/Justice League (Liga de la Justicia).jpg',
+                nombre: 'Justice League (Liga de la Justicia)',
+                descripcion: 'Motivado por la fe que había recuperado en la humanidad e inspirado por la acción altruista de Superman, Bruce Wayne recluta la ayuda de su nueva aliada, Diana Prince, para enfrentarse a un enemigo aún mayor. Juntos, Batman y Wonder Woman se mueven rápidamente para intentar encontrar y reclutar un equipo de metahumanos que combata esta nueva amenaza.',
+                precio: '10.00'
+        },
+        {
+                imagen: 'img/datosPeliculas/Serie Hunters.jpeg',
+                nombre: 'Hunters',
+                descripcion: 'Hunters es una serie de televisión del género dramático creada por Natalie Chaidez y producida por Valhalla Entertainment junto a Universal Cable Poductions para Syfy. Escrita por la misma Natalie Chaidez, esta ficción televisiva está basada en la novela escrita por el autor Whitley Strieber Alien Hunter, publicada en 2013. ',
+                precio: '5.00'
+        },
+        {
+                imagen: 'img/datosPeliculas/Serie Bruja Escarlata y Visión.jpeg',
+                nombre: 'Bruja Escarlata y Visión',
+                descripcion: 'Bruja Escarlata y Visión de Marvel Studios combina el estilo de «sitcom clásicas» con el Universo Cinematográfico de Marvel. Wanda Maximoff y Visión, dos seres súper poderosos que viven sus vidas cotidianas ideales, comienzan a sospechar que no todo es como parece.',
+                precio: '15.00'
+        },
+        {
+                imagen: 'img/datosPeliculas/Falcon y el Soldado de Invierno.jpeg',
+                nombre: 'Falcon y el Soldado de Invierno',
+                descripcion: 'Siguiendo los eventos de "Vengadores: Endgame", Sam Wilson/Falcon y Bucky Barnes/Soldado de Invierno se unen en una aventura global que pondrá a prueba sus habilidades... y su paciencia',
+                precio: '7.00'
+        },
+        {
+                imagen: 'img/datosPeliculas/La liga de la justicia El trono de Atlantis.jpeg',
+                nombre: 'La liga de la justicia: El trono de Atlantis',
+                descripcion: 'Los Villanos Orm y Manta negra hacen su jugada contra el mundo de la superficie después de la batalla con las fuerzas de Apokolips de Darkseid en La Aventura Anterior, tuvieron consecuencias mortales para Atlantis, Mera, la reina de la civilización submarina perdida busca su otro hijo, al hemanastro de Orm, Arthur Curry. ',
+                precio: '5.00'
+        },
+        {
+                imagen: 'img/datosPeliculas/Liga de la Justicia Dioses y Monstruos.jpeg',
+                nombre: 'Liga de la Justicia: Dioses y Monstruos',
+                descripcion: 'La Liga de la Justicia: Dioses y Monstruos explora una realidad recién concebida en el Universo DC, donde los miembros de la Justice League Superman, Batman y Wonder Woman son versiones mucho más oscuras de los superhéroes que las personas piensan que conocen.',
+                precio: '10.00'
+        },
+        {
+                imagen: 'img/datosPeliculas/Kung Fu Panda 4.jpg',
+                nombre: 'Kung Fu Panda 4',
+                descripcion: 'Po, quien se convertirá en el Maestro Espiritual del Valle de la Paz, busca a su sucesor como el nuevo Guerrero Dragón mientras lucha contra una nueva enemiga llamada «La Camaleona».',
+                precio: '20.00'
+        },
+        {
+                imagen: 'img/datosPeliculas/',
+                nombre: 'nombre',
+                descripcion: 'descripcion',
+                precio: '000'
+        },
+        {
+                imagen: 'img/datosPeliculas/',
+                nombre: 'nombre',
+                descripcion: 'descripcion',
+                precio: '000'
+        },
+        {
+                imagen: 'img/datosPeliculas/',
+                nombre: 'nombre',
+                descripcion: 'descripcion',
+                precio: '000'
+        },
+        {
+                imagen: 'img/datosPeliculas/',
+                nombre: 'nombre',
+                descripcion: 'descripcion',
+                precio: '000'
+        },
+        {
+                imagen: 'img/datosPeliculas/',
+                nombre: 'nombre',
+                descripcion: 'descripcion',
+                precio: '000'
+        },
+]
--- reutilizables/baseData/mejorContenido.js
+++ reutilizables/baseData/mejorContenido.js
@@ -1,34 +1,5 @@
-//@ts-check
-const contenidoData = [
-        {
-                titulo: 'Joker: Follie a deux',
-                actor: 'Joaquin Phoenix',
-                precio: '50,00',
-                enlace: 'https://es.wikipedia.org/wiki/Joker:_Folie_%C3%A0_Deux',
-                imagen: 'img/joker.png'
-        },
-        {
-                titulo: 'Deadpool 3',
-                actor: ' Ryan Reynolds',
-                precio: '25,00',
-                enlace: 'https://www.espinof.com/fecha-de-estreno/deadpool-3-fecha-estreno-trailer-reparto-pelicula-marvel-protagonizada-ryan-reynolds',
-                imagen: 'img/deadpool.png'
-        },
-        {
-                titulo: 'Venom 3',
-                actor: 'Tom Hardy',
-                precio: '100,00',
-                enlace: 'https://www.fotogramas.es/noticias-cine/a37978794/venom-3-fecha-estreno-argumento-reparto-trailer/',
-                imagen: 'img/venom.png'
-        },
-        {
-                titulo: 'Wonder Woman 3',
-                actor: 'Gal Gadot',
-                precio: '70,00',
-                enlace: 'https://www.fotogramas.es/noticias-cine/a37983052/wonder-woman-3-gal-gadot-sinopsis-fecha-estreno-reparto-trailer/',
-                imagen: 'img/wonderwoman.png'
-        }
-]
+import { contenidoData } from "./datos.js";
+import { PeliculasData } from "./datos.js";
 
 class mejorContenido {
         constructor(titulo, actor, precio, enlace,imagen){
@@ -41,3 +12,14 @@ class mejorContenido {
 }
 
 export const recorrerMejorContenido = contenidoData.map(x => new mejorContenido(x.titulo, x.actor, x.precio, x.enlace, x.imagen));
+
+class peliculasContenido {
+        constructor(imagen, nombre, descripcion, precio){
+                this.imagen = imagen;
+                this.nombre = nombre;
+                this.descripcion = descripcion;
+                this.precio = precio;
+        }
+}
+
+export const recorrerPeliculasContenido = PeliculasData.map(y => new peliculasContenido(y.imagen, y.nombre, y.descripcion, y.precio));
