6c8e1bbc8b287f318af44547629b4703a92c7eff
Erick Paredes Win <erikparedes246@gmail.com>
2024-03-20 11:23:20 -0500

    creando bace de datos
    js

--- reutilizables/baseData/datos.js
+++ reutilizables/baseData/datos.js
@@ -78,33 +78,98 @@ export const PeliculasData = [
                 precio: '20.00'
         },
         {
-                imagen: 'img/datosPeliculas/',
-                nombre: 'nombre',
-                descripcion: 'descripcion',
-                precio: '000'
+                imagen: 'img/datosPeliculas/Pobres criaturas.jpeg',
+                nombre: 'Pobres criaturas',
+                descripcion: 'Pobres criaturas, la reinvención en clave de comedia negra de la historia de Frankenstein del director griego Yorgos Lanthimos, cuenta la historia de Bella Baxter (Emma Stone) y el científico que experimenta con ella: el Dr. Godwin Baxter, interpretado por nada más y nada menos que don Willem Dafoe.',
+                precio: '30.00'
         },
         {
-                imagen: 'img/datosPeliculas/',
-                nombre: 'nombre',
-                descripcion: 'descripcion',
-                precio: '000'
+                imagen: 'img/datosPeliculas/Cazafantasmas Imperio helado.jpeg',
+                nombre: 'Cazafantasmas: Imperio helado',
+                descripcion: 'Las escenas poscréditos de Cazafantasmas: Más allá (2021) revelaba que Winston (Ernie Hudson) ahora era el dueño de la antigua estación de bomberos de los cazafantasmas. En Cazafantasmas: Imperio helado regresamos a Nueva York para luchar contra nuevas entidades ectoplásmicas.',
+                precio: '5.00'
         },
         {
-                imagen: 'img/datosPeliculas/',
-                nombre: 'nombre',
-                descripcion: 'descripcion',
-                precio: '000'
+                imagen: 'img/datosPeliculas/Godzilla x Kong The New Empire.jpeg',
+                nombre: 'Godzilla x Kong: The New Empire',
+                descripcion: 'La secuela de Godzilla vs. Kong, que será la quinta entrega del "monsterverse" tras ésta, Godzilla (2014), Kong, la Isla Calavera (2017) y Godzilla, Rey de los Monstruos (2019), comenzó su producción en verano de 2022 en Australia. Por primera vez en la franquicia, un director repetirá a los mandos de una nueva película. Adam Wingard volverá la silla de dirección y los primeros rumores apuntaban a que Godzilla x Kong: The New Empire podría adaptar la historia de El hijo de Kong (Ernest B. Schoedsack, 1933), pero los primeros avances parecen presentar un nuevo y extraño simio como villano del título.',
+                precio: '10.00'
         },
         {
-                imagen: 'img/datosPeliculas/',
-                nombre: 'nombre',
-                descripcion: 'descripcion',
-                precio: '000'
+                imagen: 'img/datosPeliculas/El reino del planeta de los simios,jpeg',
+                nombre: 'El reino del planeta de los simios',
+                descripcion: '"Puedo asegurar es que el legado de César continuará", sentenció en redes Wes Ball, director de un proyecto que inicia un capítulo nuevo en la saga, comenzando muchos años después de la conclusión de La guerra del planeta de los simios de 2017. Los monos vuelven con Disney al mando con El reino del planeta de los simios',
+                precio: '6.00'
         },
         {
-                imagen: 'img/datosPeliculas/',
-                nombre: 'nombre',
-                descripcion: 'descripcion',
-                precio: '000'
+                imagen: 'img/datosPeliculas/Del revés 2.jpeg',
+                nombre: 'Del revés 2',
+                descripcion: 'La secuela de Del revés, la película de Pixar ganadora del Óscar a mejor película de animación en 2015, es una realidad. Los nuevos invitados en la mente de Riley serán Envidia, Hastío, Ansiedad y Vergüenza. Estas nuevas emociones llegarán al centro de control que conocimos en la primera cinta y estamos seguros de que pondrán todo patas arriba en Del revés 2 (Inside Out 2)',
+                precio: '30.00'
+        },
+]
+export const juegosData = [
+        {
+                imagen: 'img/juegosData/Palworld.jpg',
+                nombre: 'Palworld',
+                descripcion: 'Palworld, desarrollado por Pocketpair, es una aventura para un jugador y con multijugador con mundo abierto, supervivencia y creación que nos permite coleccionar y domar unas criaturas llamadas Pal. Esta descripción lógicamente hace que sea comparado con Pokémon de Nintendo, la veterana saga de rol y coleccionismo.',
+                genero: 'Mundo abierto / Supervivencia',
+                precio: '15.00'
+        },
+        {
+                imagen: 'img/juegosData/The Last of Us Parte II Remasterizado.jpg',
+                nombre: 'The Last of Us Parte II Remasterizado',
+                descripcion: 'The Last of Us 2 es la secuela de The Last of Us, uno de los juegos más aclamados de la consola PS3, que apareció en 2015 en PS4 en versión remasterizada. Esta continuación ha sido anunciada en el evento PlayStation Experience de diciembre de 2016, generando grandísimas expectativas entre los jugadores.',
+                genero: 'Aventura de acción / Survival Horror',
+                precio: '14.00'
+        },
+        {
+                imagen: 'img/juegosData/The Universim.png',
+                nombre: 'The Universim',
+                descripcion: 'The Universim es un videojuego de gestión y estrategia enmarcado dentro del God Game en el que, dejando a un lado las granjas y las ciudades, se nos permitirá administrar nuestro propio planeta como un creador y Dios omniscente. El título arranca en la Edad de Piedra y nos ofrecerá ir avanzando poco a poco a través de las distintas civilizaciones.',
+                genero: 'Estrategia en tiempo real / Simulador de vida',
+                precio: '10.00'
+        },
+        {
+                imagen: 'img/juegosData/Tekken 8.jpg',
+                nombre: 'Tekken 8',
+                descripcion: 'Tekken 8 es la octava entrega de la saga de lucha en 3D de Bandai Namco para Xbox Series, PS5 y PC, y continuará la historia de su predecesor, ambientándose seis meses después del final de su modo historia, prometiendo ser una revolución en la saga a varios niveles además de plantear un nuevo capítulo en su tormentosa historia de los Mishima.',
+                genero: 'Lucha / Lucha 3D',
+                precio: '28.00'
+        },
+        {
+                imagen: 'img/juegosData/Like a Dragon Infinite Wealth.jpg',
+                nombre: 'Like a Dragon: Infinite Wealth ',
+                descripcion: 'Like a Dragon: Infinite Wealth es un JRPG desarrollado por Ryu Ga Gotoku Studio y publicado por SEGA para PlayStation 4, PlayStation 5, Xbox One, Xbox Series y PC. La octava entrega de la saga Yakuza vuelve a apostar por un sistema de combate por turnos y está protagonizado por el simpático Ichiban Kasuga, el nuevo héroe de la serie, y con el retorno del mítico Kazuma Kiryu, en el ocaso de su vida.',
+                genero: 'Aventura de acción / Tipo GTA',
+                precio: '9.00'
+        },
+        {
+                imagen: 'img/juegosData/Granblue Fantasy Relink.png',
+                nombre: 'Granblue Fantasy: Relink',
+                descripcion: 'Granblue Fantasy: Relink es un videojuego de rol y acción japonés, un spinoff de la saga Granblue Fantasy, un exitoso gacha para móviles. Desarrollado por Cygames, llegará PlayStation 4, PlayStation 5 y PC, nos llevará a vivir una gran aventura a lo largo y ancho de varias islas flotantes, sumergiéndonos en una épica historia cargada de personajes carismáticos y criaturas fantásticas.',
+                genero: 'Action-RPG',
+                precio: '17.00'
+        },
+        {
+                imagen: 'img/juegosData/SILENT HILL The Short Message.jpg',
+                nombre: 'GSILENT HILL: The Short Message',
+                descripcion: 'SILENT HILL: The Short Message es un juego de terror desarrollado por Konami y HexaDrive y publicado por Konami para PlayStation 5. Después de muchos años de descanso la mítica saga de terror vuelve con una nueva entrega, en esta ocasión desde una perspectiva en primera persona y de manera gratuita, con una experiencia corta pero intensa, que nos mete de nuevo el miedo en el cuerpo y que ha contado con el trabajo de Masahiro Ito diseñando a una terrorífica criatura y Akira Yamaoka a cargo de la música.',
+                genero: 'Aventura de acción / Survival Horror / Terror',
+                precio: '3.00'
+        },
+        {
+                imagen: 'img/juegosData/Suicide Squad Kill the Justice League.jpg',
+                nombre: 'Suicide Squad: Kill the Justice League',
+                descripcion: 'Suicide Squad: Kill the Justice League es el nuevo título de acción cooperativa y en mundo abierto desarrollado por Rocksteady y Warner Bros. que puede disfrutarse solo o en equipo en línea para hasta cuatro jugadores. Su historia es original -no tiene relación con películas o cómics- y nos lleva a una versión de mundo abierto de la ciudad de Metrópolis.',
+                genero: 'Aventura de acción / Mundo abierto',
+                precio: '6.00'
+        },
+        {
+                imagen: 'img/juegosData/Persona 3 Reload.jpg',
+                nombre: 'Persona 3 Reload',
+                descripcion: 'Persona 3 Reload es un JRPG desarrollado por Atlus y publicado por SEGA para PlayStation 4, PlayStation 5, Xbox One, Xbox Series y PC. Uno de los juegos de rol japonés más aclamados de todos los tiempos regresa con este espectacular remake que actualiza los gráficos al estilo de Persona 5, además de añadir diversas novedades tanto en la narrativa como en el gameplay.',
+                genero: 'JRPG / Rol',
+                precio: '17.00'
         },
 ]
